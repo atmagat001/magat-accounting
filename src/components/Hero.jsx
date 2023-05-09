00d36ff8857d10046d5c3a5c8315344b7ec9670a
Alvin Magat <atmagat@gmail.com>
2023-05-09 10:13:48 +0800

    initial commit to tailwind version

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,10 +3,15 @@ import React from "react";
 
 function Hero() {
   return (
-    <div className="hero">
-      <h1>Magat Accounting Services</h1>
-      <p>Lets Grow Your Business.</p>
+    <div className="hero-container">
+        <div className="hero">
+        </div>
+            <div className="content">
+            <h1>Magat Accounting Services</h1>
+            <p>Lets Grow Your Business.</p>
     </div>
+    
+</div>
   );
 }
 
